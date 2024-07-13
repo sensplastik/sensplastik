1cea7b4138d502caf30552034b67f9ebde8b52c4
L Houcine  Cherif <31718137+lhoucinecherif@users.noreply.github.com>
2024-07-14 00:18:31 +0100

    update hamburger

--- components/global/Navbar/Hamburger.tsx
+++ components/global/Navbar/Hamburger.tsx
@@ -1,26 +1,29 @@
 import './Hamburger.scss';
-
 import { cva } from 'cva';
 import { memo } from 'react';
 
+// Define component styles using cva
 const componentStyles = cva('hamburger', {
   variants: {
     intent: {
       open: 'hamburger--open',
-      close: 'hamburger--close',
-      hide: 'hamburger--hide',
-      show: 'hamburger--show'
+      close: '', // Optionally, you might want to specify a default class here or omit if not needed
     },
+  },
+  defaultVariants: {
+    intent: 'close', // Default to 'close' variant if no `intent` is provided
   },
 });
 
+// Define props interface with the correct type for `state`
 interface HamburgerProps {
-  state?:string
+  state?: 'open' | 'close'; // Restrict to 'open' or 'close' values
 }
 
-function Hamburger({state = 'show'}: HamburgerProps) {
+// Functional component
+function Hamburger({ state = 'close' }: HamburgerProps) {
   return (
-    <div className={componentStyles({ intent: state  })}>
+    <div className={componentStyles({ intent: state })}>
       <div className="hamburger__line"></div>
       <div className="hamburger__line"></div>
     </div>
